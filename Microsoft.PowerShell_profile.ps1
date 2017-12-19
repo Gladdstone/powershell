988dc8df2a427e7d54be2d2f4c6fb3a7aae0d757
Joe Farrell <jdf4921@rit.edu>
2017-12-19 00:30:06 -0500

    Outputs welcome message and basic weather information

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,20 +7,31 @@ if($host.UI.RawUI.WindowTitle -like "*administrator*") {
 # Aliases
 new-item alias:np -value 'C:\Program Files (x86)\Notepad++\notepad++.exe'
 
+# Welcome message
+$welcome = "Welcome, Mr. Farrell"
+
 # Set city to query for weather
 $city = ""
-$api_url = "https://api.openweathermap.org/data/2.5/weather?q=" + $city + "&APPID="
+$api_url = "https://api.openweathermap.org/data/2.5/weather?q=" + $city + "&units=imperial&APPID="
 
 # Get weather from https://openweathermap.org/api
 $weatherJson = Invoke-RestMethod -Uri $api_url
 
 # Parse JSON object for relevant data
-$clouds = $weatherJson.weather.description
-$temp = $weatherJson.main.temp * (9 / 5) - 459.67		# Temperature is passed in Kelvin, converted to Fahrenheit
+$weather = $weatherJson.weather.description
+$temp = $weatherJson.main.temp
 $pressure = $weatherJson.main.pressure
 $humidity = $weatherJson.main.humidity
-$min_temp = $weatherJson.main.temp_min * (9 / 5) - 459.67
-$max_temp = $weatherJson.main.temp_max * (9 / 5) - 459.67
+$min_temp = $weatherJson.main.temp_min
+$max_temp = $weatherJson.main.temp_max
+$sunrise_mill = $weatherJson.sys.sunrise					# Sent in unix time
+$sunset_mill = $weatherJson.sys.sunset
+$sunrise = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($sunrise_mill))
+$sunset = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($sunset_mill))
+
+Clear-Host
+Write-Host "`n"$welcome
+Write-Host "The weather today is" $weather "`n And the current temperature is "$temp
 
 # This takes a long time
 # Update-Help
