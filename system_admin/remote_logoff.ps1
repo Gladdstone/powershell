3f0d64096f09339199f5e25be73712db8196a503
Joe Farrell <disastrousgamemaster@gmail.com>
2018-08-16 15:44:37 -0400

    Remote logoff now works for PCs with non-matching usernames

--- system_admin/remote_logoff.ps1
+++ system_admin/remote_logoff.ps1
@@ -5,6 +5,7 @@
 #>
 $server = $args[0];
 $username = $env:USERNAME;
-$session = ((quser /server:$server | ? { $_ -match $username }) " +")[2];
+#$session = ((quser /server:$server | ? { $_ -match $username }) " +")[2];
+$session = (quser /server:$server | Select-Object -Index 1 | % { $_.Split(" ", [System.StringSplitOptions]::RemoveEmptyEntries) })[1];
 
 logoff $session /server:$server;
